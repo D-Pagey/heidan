25a3e1300d47313fd45bbfcb576a5709e3071c0b
Dan Page <dan.page91@gmail.com>
2020-07-01 22:37:09 +0100

    build(majors): update all packages

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,36 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Header from '../Header';
-import logo from './assets/logo.svg';
-import * as S from './styles';
-import './styles.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Header from "../Header";
+import logo from "./assets/logo.svg";
+import * as S from "./styles";
+import "./styles.css";
 
 const App = () => {
-  const [joke, setJoke] = useState('joke incoming...');    
+  const [joke, setJoke] = useState("joke incoming...");
 
   const getJoke = async () => {
-    const { data } = await axios.get(`https://heidan.herokuapp.com/joke`);    
+    const { data } = await axios.get(`https://heidan.herokuapp.com/joke`);
     setJoke(data.joke);
-  }
+  };
 
   useEffect(() => {
     getJoke();
-  }, [])
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-      <Header />
-      <S.Wrapper>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Heidan</h1>
-        
-        <p>{joke}</p>
+        <Header />
+        <S.Wrapper>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Heidan</h1>
 
-    </S.Wrapper>
+          <p>{joke}</p>
+        </S.Wrapper>
       </header>
     </div>
   );
-}
+};
 
 export default App;
