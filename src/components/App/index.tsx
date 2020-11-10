758b33aa4cc1a21485962a93e44aa4e17d510c0d
Dan Page <dan.page91@gmail.com>
2020-11-10 21:20:51 +0000

    add CRD functionality for new activity endpoint

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -6,16 +6,68 @@ import * as S from "./styles";
 import "./styles.css";
 
 const App = () => {
-  const [joke, setJoke] = useState("joke incoming...");
+  const [activities, setActivities] = useState<{ name: string; id: string }[]>(
+    []
+  );
+  const [description, setDescription] = useState("");
+  const [name, setName] = useState("");
+  const [cost, setCost] = useState("");
 
-  const getJoke = async () => {
-    const { data } = await axios.get(`https://heidan.herokuapp.com/joke`);
-    setJoke(data.joke);
+  const getActivities = async () => {
+    const { data } = await axios.get(
+      `https://heidan-api.herokuapp.com/activities`
+    );
+    console.log({ data });
+    setActivities(data);
   };
 
   useEffect(() => {
-    getJoke();
+    getActivities();
   }, []);
+
+  const submitActivity = async () => {
+    try {
+      const response = await axios.post(
+        `https://heidan-api.herokuapp.com/activity`,
+
+        {
+          description,
+          name,
+          cost,
+          complete: false,
+          location: "England",
+        }
+      );
+      console.log({ response });
+
+      getActivities();
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    setCost("");
+    setDescription("");
+    setName("");
+
+    submitActivity();
+  };
+
+  const handleDelete = (id: string) => async () => {
+    try {
+      const response = await axios.delete(
+        `https://heidan-api.herokuapp.com/activity/${id}`
+      );
+      console.log({ response });
+
+      getActivities();
+    } catch (error) {
+      console.log({ error });
+    }
+  };
 
   return (
     <div className="App">
@@ -25,7 +77,41 @@ const App = () => {
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Heidan</h1>
 
-          <p>{joke}</p>
+          <ul>
+            {activities.map((activity) => (
+              <div key={activity.name}>
+                <li>{activity.name}</li>
+                <button type="button" onClick={handleDelete(activity.id)}>
+                  DELETE
+                </button>
+              </div>
+            ))}
+          </ul>
+
+          <form onSubmit={handleSubmit}>
+            <label>Description</label>
+            <input
+              type="text"
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
+            />
+
+            <label>Name</label>
+            <input
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
+
+            <label>Cost</label>
+            <input
+              type="number"
+              onChange={(e) => setCost(e.target.value)}
+              value={cost}
+            />
+
+            <button type="submit">Submit</button>
+          </form>
         </S.Wrapper>
       </header>
     </div>
